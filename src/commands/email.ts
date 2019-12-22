9e23efd38212b7686d90f5c8be23d7463c15d910
geisterfurz007 <michelvonv@me.com>
2019-12-22 01:56:00 +0100

    Added missing parenthesis

--- src/commands/email.ts
+++ src/commands/email.ts
@@ -5,7 +5,7 @@ import Command from './Command';
 const Email: Command = {
 	action: () => {
 		['The first part of this domain at google\'s mail server',
-			'(if you are not a bot, you should be able to piece that one together'].map(Line.out).forEach(CurrentLinesStore.addLine);
+			'(if you are not a bot, you should be able to piece that one together)'].map(Line.out).forEach(CurrentLinesStore.addLine);
 	},
 	description: 'Shows my mail address (please don\'t spam...)',
 	name: 'email',

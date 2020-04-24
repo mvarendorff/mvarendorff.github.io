9f1e2f8afe7063a64e23d2ba1d0966a896e75b33
geisterfurz007 <michelvonv@me.com>
2020-04-24 17:47:09 +0200

    Updates

---
+++ precache-manifest.95b8c69cd29f0d724f21d36143f00d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5beef03e978178f8d9a42e0aab36f342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "534e36d0b6d3a7c84426",
+    "url": "/static/css/main.7438c3af.chunk.css"
+  },
+  {
+    "revision": "a116aaa39ad48c3b8f9e",
+    "url": "/static/js/2.953fa583.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.953fa583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534e36d0b6d3a7c84426",
+    "url": "/static/js/main.c2fa0ed2.chunk.js"
+  },
+  {
+    "revision": "91dac7abd3378bb27a1d",
+    "url": "/static/js/runtime-main.eb8f8609.js"
+  }
+]);
